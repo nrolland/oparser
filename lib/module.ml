e848534caa41ba6bb7efebc0b764200c581c37a2
nicolas rolland <nicolas.roland@gmail.com>
2018-11-10 12:53:45 +0100

    fix

--- lib/module.ml
+++ lib/module.ml
@@ -1,10 +1,5 @@
 open MParser
 
-type nonrec ('a, 's) operator =
-    Infix of (('a -> 'a -> 'a, 's) t * assoc)
-  | Prefix of ('a -> 'a, 's) t
-  | Postfix of ('a -> 'a, 's) t
-
 let infix p op = Infix (p |>> (fun _ a b -> (`Binop (op, a, b))), Assoc_left)
 
 let operators =
@@ -21,10 +16,7 @@ let operators =
 
 let decimal = many1_chars digit |>> int_of_string
 
-let term    = decimal |>> fun i -> `Int i
-
-let expr =
-    expression operators (decimal |>> fun i -> `Int i)
+let expr = expression operators (decimal |>> fun i -> `Int i)
 
 let rec calc = function
   | `Int i -> i
